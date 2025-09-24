e3d97683c9da8c9945d225304cf420f2fdb30ba2
Вован <@DESKTOP-UONI60D>
2025-09-24 16:04:45 +0300

    Добавьте файлы проекта.

---
+++ dm/View/Window/Administrator.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace dm.View.Window
+{
+    /// <summary>
+    /// Логика взаимодействия для Administrator.xaml
+    /// </summary>
+    public partial class Administrator : System.Windows.Window
+    {
+        public Administrator()
+        {
+            InitializeComponent();
+        }
+
+        private void AddEditUserBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ dm/View/Window/Authorization.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace dm.View.Window
+{
+    /// <summary>
+    /// Логика взаимодействия для Authorization.xaml
+    /// </summary>
+    public partial class Authorization : System.Windows.Window
+    {
+        public Authorization()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
